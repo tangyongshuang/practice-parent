9096ff7c74cf355615e9f1c609aeed27267da4bb
albert.tang <albert.tang@circle.us>
2019-06-05 23:11:16 +0800

    add more zuul properties info

--- api-gateway/src/main/java/com/anhe/APIGateway.java
+++ api-gateway/src/main/java/com/anhe/APIGateway.java
@@ -1,8 +1,10 @@
 package com.anhe;
 
+import com.anhe.filter.AccessFilter;
 import org.springframework.boot.SpringApplication;
 import org.springframework.cloud.client.SpringCloudApplication;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
+import org.springframework.context.annotation.Bean;
 
 
 @EnableZuulProxy
@@ -10,5 +12,10 @@ import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 public class APIGateway {
     public static void main(String[] args) {
         SpringApplication.run(APIGateway.class, args);
+    }
+
+    @Bean
+    public AccessFilter accessFilter(){
+        return new AccessFilter();
     }
 }
